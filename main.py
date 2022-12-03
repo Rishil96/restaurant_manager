330298f7f15fa9ba4e62db217296a9286866b4ca
Rishil Ramesh <rishil.ramesh@outlook.com>
2022-12-03 15:05:25 +0530

    Add : Add default values to the items

--- main.py
+++ main.py
@@ -94,9 +94,13 @@ dessert_list = ["Ice cream", "Fruit", "Brownies", "Pudding", "Cheesecake", "Cake
 
 # Create food items
 food_items = []
+food_box = []
+food_text = []
 food_counter = 0
 
 for food in food_list:
+
+    # Create checkboxes
     food_items.append("")
     food_items[food_counter] = tkinter.IntVar()
     food_checkbox = tkinter.Checkbutton(food_panel,
@@ -106,13 +110,32 @@ for food in food_list:
                                         offvalue=0,
                                         variable=food_items[food_counter])
     food_checkbox.grid(row=food_counter, column=0, sticky=tkinter.W)
+
+    # Create Input boxes
+    food_box.append("")
+    food_text.append("")
+    food_text[food_counter] = tkinter.StringVar()
+    food_text[food_counter].set('0')
+
+    food_box[food_counter] = tkinter.Entry(food_panel,
+                                           font=("Dosis", 19),
+                                           bd=1,
+                                           width=6,
+                                           state=tkinter.DISABLED,
+                                           textvariable=food_text[food_counter])
+    food_box[food_counter].grid(row=food_counter, column=1)
+
     food_counter += 1
 
 # Create drink items
 drink_items = []
+drink_box = []
+drink_text = []
 drink_counter = 0
 
 for drink in drinks_list:
+
+    # Create checkboxes
     drink_items.append("")
     drink_items[drink_counter] = tkinter.IntVar()
     drink_checkbox = tkinter.Checkbutton(drink_panel,
@@ -122,13 +145,32 @@ for drink in drinks_list:
                                          offvalue=0,
                                          variable=drink_items[drink_counter])
     drink_checkbox.grid(row=drink_counter, column=0, sticky=tkinter.W)
+
+    # Create Input boxes
+    drink_box.append("")
+    drink_text.append("")
+    drink_text[drink_counter] = tkinter.StringVar()
+    drink_text[drink_counter].set('0')
+
+    drink_box[drink_counter] = tkinter.Entry(drink_panel,
+                                             font=("Dosis", 19),
+                                             bd=1,
+                                             width=6,
+                                             state=tkinter.DISABLED,
+                                             textvariable=drink_text[drink_counter])
+    drink_box[drink_counter].grid(row=drink_counter, column=1)
+
     drink_counter += 1
 
 # Create dessert items
 dessert_items = []
+dessert_box = []
+dessert_text = []
 dessert_counter = 0
 
 for dessert in dessert_list:
+
+    # Create checkboxes
     dessert_items.append("")
     dessert_items[dessert_counter] = tkinter.IntVar()
     dessert_checkbox = tkinter.Checkbutton(dessert_panel,
@@ -138,6 +180,21 @@ for dessert in dessert_list:
                                            offvalue=0,
                                            variable=dessert_items[dessert_counter])
     dessert_checkbox.grid(row=dessert_counter, column=0, sticky=tkinter.W)
+
+    # Create Input boxes
+    dessert_box.append("")
+    dessert_text.append("")
+    dessert_text[dessert_counter] = tkinter.StringVar()
+    dessert_text[dessert_counter].set('0')
+
+    dessert_box[dessert_counter] = tkinter.Entry(dessert_panel,
+                                                 font=("Dosis", 19),
+                                                 bd=1,
+                                                 width=6,
+                                                 state=tkinter.DISABLED,
+                                                 textvariable=dessert_text[dessert_counter])
+    dessert_box[dessert_counter].grid(row=dessert_counter, column=1)
+
     dessert_counter += 1
 
 application.mainloop()
